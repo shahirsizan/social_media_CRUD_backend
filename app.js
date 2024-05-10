042efaaeac77de7194ce4672de779547ecc1d6ed
shahirsizan <shahir.sizan18@gmail.com>
2024-05-10 15:22:28 +0600

    Added mongodb connection setup code

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api", (req, res, next) => {
-	res.send("Hello sizan");
+mongoose.connect("mongodb://localhost:27017/social_media_CRUD_backend");
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+	console.log("Connected to MongoDB");
+	app.listen(5000);
 });
-
-app.listen(5000);
